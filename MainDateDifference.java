5e0f00320a15623a307ba5a2de61c0fed11f011a
unknown <esmajic@yahoo.com>
2019-05-05 17:42:49 +0200

    initial commit

---
+++ MainDateDifference.java
@@ -0,0 +1,40 @@
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+import java.util.Scanner;
+
+public class MainDateDifference {
+
+	public static void main(String[] args) throws ParseException {
+		
+		Scanner input = new Scanner(System.in);
+
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+		System.out.println("Enter older date (dd-MM-yyyy):  ");
+		String date1 = input.nextLine() ;
+		System.out.println("Enter later date (dd-MM-yyyy):  ");
+		String date2 = input.nextLine() ;
+		
+		try {
+			Date newDate1 = dateFormat.parse(date1);
+			System.out.println(newDate1);
+			Date newDate2 = dateFormat.parse(date2);
+			System.out.println(newDate2);
+			
+			System.out.println(DateDifference.getDateDiff(newDate1, newDate2, TimeUnit.DAYS) + " days.");
+			
+		} catch (ParseException e) {
+			
+			e.printStackTrace();
+		}
+		
+		
+		
+		input.close();
+		
+		
+	}
+
+}
