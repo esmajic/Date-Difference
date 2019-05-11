2d52bf0c3f5c412e362edbc559b41aaa42106e0f
unknown <esmajic@yahoo.com>
2019-05-11 17:44:15 +0200

    added new class Datum and some addition to main method, added list

--- MainDateDifference.java
+++ MainDateDifference.java
@@ -1,40 +1,47 @@
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 import java.util.Scanner;
 
 public class MainDateDifference {
 
+	public static ArrayList<Datum> date = new ArrayList<>();
+
 	public static void main(String[] args) throws ParseException {
-		
+
 		Scanner input = new Scanner(System.in);
 
 		SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
 		System.out.println("Enter older date (dd-MM-yyyy):  ");
-		String date1 = input.nextLine() ;
-		System.out.println("Enter later date (dd-MM-yyyy):  ");
-		String date2 = input.nextLine() ;
-		
+		String date1 = input.nextLine();
+		System.out.println("Enter present date (dd-MM-yyyy):  ");
+		String date2 = input.nextLine();
+		Datum dates = new Datum(date1, date2);
+
+		date.add(dates);
+
 		try {
 			Date newDate1 = dateFormat.parse(date1);
 			System.out.println(newDate1);
 			Date newDate2 = dateFormat.parse(date2);
 			System.out.println(newDate2);
-			
-			System.out.println("Difference between dates is " + DateDifference.getDateDiff(newDate1, newDate2, TimeUnit.DAYS) + " days.");
-			
+
+			System.out.println("Difference between dates is "
+					+ DateDifference.getDateDiff(newDate1, newDate2, TimeUnit.DAYS) + " days.");
+			if (DateDifference.getDateDiff(newDate1, newDate2, TimeUnit.DAYS) > 30) {
+				System.out.println("Difference in days is greater than 30 days!");
+			}
+
 		} catch (ParseException e) {
-			
+
 			e.printStackTrace();
 		}
-		
-		
-		
+
 		input.close();
-		
-		
+
 	}
 
 }
